db228dd8c96e6b100c0c54b5f77998ab9a528f29
Kalpeshkumavat1 <kalpeshkumavat2772@gmail.com>
2026-01-11 11:55:03 +0530

    update Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
       ? process.env.SOCKET_IO_CORS_ORIGIN.split(',')
       : process.env.CORS_ORIGIN
         ? process.env.CORS_ORIGIN.split(',')
-        : ['http://localhost:5173', 'http://localhost:3000'],
+        : ['http://localhost:5173', 'http://localhost:3000','https://videohub-pulse.netlify.app'],
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -43,7 +43,7 @@ app.use(compression());
 app.use(cors({
   origin: process.env.CORS_ORIGIN 
     ? process.env.CORS_ORIGIN.split(',')
-    : ['http://localhost:5173', 'http://localhost:3000'],
+    : ['http://localhost:5173', 'http://localhost:3000','https://videohub-pulse.netlify.app'],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
